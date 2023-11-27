09f6c57fc4cb1e033bb51f7c260443545c922341
Максим <116649316+ParamonovMO@users.noreply.github.com>
2023-11-27 09:18:20 +0500

    Update test_main.py

--- pytest_learning/tests/test_main.py
+++ pytest_learning/tests/test_main.py
@@ -1,14 +1,19 @@
-def set_ep():
+import pytest
+
+@pytest.fixture() #действие до
+def auth():
     print('Вход в систему выполнен')
 
 
-def test_sending_mail_1():
-    print('Письмо отправлено')
+def test_sending_mail_1(auth):
+    print('Первое письмо отправлено')
 
 
-def test_sending_mail_2():
-    print('Письмо отправлено')
+def test_sending_mail_2(auth):
+    print('Второе письмо отправлено')
 
 
-def test_sending_mail_3():
-    print('Письмо отправлено')
+def test_sending_mail_3(auth):
+    print('Третье письмо отправлено')
+
+#Запуск pytest -s -v
