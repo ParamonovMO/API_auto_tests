2738a7610912549baf8fd820d99b31e6c1071e06
Максим <116649316+ParamonovMO@users.noreply.github.com>
2023-11-27 11:24:36 +0500

    Update test_main.py

--- pytest_learning/tests/test_main.py
+++ pytest_learning/tests/test_main.py
@@ -1,14 +1,14 @@
 import pytest
 
-def test_sending_mail_1(auth):
+def test_sending_mail_1(auth, some):
     print('Первое письмо отправлено')
 
 
-def test_sending_mail_2(auth):
+def test_sending_mail_2(auth, some):
     print('Второе письмо отправлено')
 
 
-def test_sending_mail_3(auth):
+def test_sending_mail_3(auth, some):
     print('Третье письмо отправлено')
 
 #Запуск pytest -s -v
