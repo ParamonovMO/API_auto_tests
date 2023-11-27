41a99969cd26614fbf54ac9af8f46d1c9ed850e8
Максим <116649316+ParamonovMO@users.noreply.github.com>
2023-11-27 13:54:56 +0500

    Update test_main.py

--- pytest_learning/tests/test_main.py
+++ pytest_learning/tests/test_main.py
@@ -11,6 +11,6 @@ def test_sending_mail_2(auth, some):
 def test_sending_mail_3(auth, some):
     print('Третье письмо отправлено')
 
-#Запуск pytest -s -v
+#Запуск pytest -s -v test_main.py
 #-s - Отображение результата работы функций
 #-v - Отображение файла, функций и результата теста
