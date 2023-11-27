4483ade3724dac8e0c8727aff0a496daee3309df
Максим <116649316+ParamonovMO@users.noreply.github.com>
2023-11-27 11:27:21 +0500

    Update conftext.py

--- pytest_learning/tests/conftext.py
+++ pytest_learning/tests/conftext.py
@@ -3,6 +3,8 @@ import pytest
 @pytest.fixture() #действие до
 def auth():
     print('Вход в систему выполнен')
+    yield
+    print('Выход из системы')
 
 @pytest.fixture(scope='module')
 def some():
